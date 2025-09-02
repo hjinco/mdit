8889fc02dcced0caf2f4b35ce905b4dc4f543bb9
hhhjin <kkp7125@gmail.com>
2025-09-02 09:12:34 +0900

    Implement edit, help, view, and window menus with predefined items

---
+++ src/components/window-menu/menu/window-menu.ts
@@ -0,0 +1,25 @@
+import { PredefinedMenuItem, Submenu } from '@tauri-apps/api/menu'
+
+export async function createWindowMenu() {
+  return await Submenu.new({
+    text: 'Window',
+    items: [
+      await PredefinedMenuItem.new({
+        text: 'Minimize',
+        item: 'Minimize',
+      }),
+      await PredefinedMenuItem.new({
+        text: 'Maximize',
+        item: 'Maximize',
+      }),
+      await PredefinedMenuItem.new({
+        text: 'Separator',
+        item: 'Separator',
+      }),
+      await PredefinedMenuItem.new({
+        text: 'Close Window',
+        item: 'CloseWindow',
+      }),
+    ],
+  })
+}
