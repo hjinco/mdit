f05cdbf78be4ff120eceac1d831a59324e16ecac
hhhjin <47374619+hhhjin@users.noreply.github.com>
2025-10-28 12:39:47 +0900

    enhance style (#110)

--- src/components/editor/ui/node-blockquote.tsx
+++ src/components/editor/ui/node-blockquote.tsx
@@ -4,7 +4,7 @@ export function BlockquoteElement(props: PlateElementProps) {
   return (
     <PlateElement
       as="blockquote"
-      className="my-1 border-l-2 pl-6 italic"
+      className="my-1 border-l-3 border-primary pl-6 italic"
       {...props}
     />
   )
