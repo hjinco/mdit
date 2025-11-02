18c4bbcb65387136c9bfa1a4c88e4b046ecd8cf7
hhhjin <kkp7125@gmail.com>
2025-11-02 12:16:29 +0900

    style

--- src/components/file-explorer/ui/workspace-dropdown.tsx
+++ src/components/file-explorer/ui/workspace-dropdown.tsx
@@ -39,7 +39,7 @@ export function WorkspaceDropdown({
           size="sm"
           className="w-full justify-start hover:bg-stone-200/80 dark:hover:bg-stone-700/80 text-foreground/90 font-semibold tracking-tight"
         >
-          <InboxIcon className="text-orange-600/80 bg-orange-600/10 rounded-sm" />
+          <InboxIcon />
           <span className="truncate">{currentWorkspaceName}</span>
           <ChevronDown className="ml-auto" />
         </Button>
