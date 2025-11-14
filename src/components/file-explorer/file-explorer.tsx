22114f6a258cfecf1c5f5d66175764e8cb58589c
hhhjin <kkp7125@gmail.com>
2025-11-14 11:30:38 +0900

    fix: display file-explorer resize bar on hover

--- src/components/file-explorer/file-explorer.tsx
+++ src/components/file-explorer/file-explorer.tsx
@@ -388,7 +388,7 @@ export function FileExplorer() {
         </footer>
         {isOpen && (
           <div
-            className="absolute top-0 -right-0.5 z-10 h-full w-1 cursor-col-resize bg-transparent"
+            className="absolute top-0 -right-1 z-10 h-full w-1.5 cursor-col-resize bg-transparent hover:bg-foreground/20 transition-colors delay-100"
             onPointerDown={handlePointerDown}
           />
         )}
