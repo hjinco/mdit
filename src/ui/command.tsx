9310383fb8a1ec797737dd28c61c2b7091bee5ef
hhhjin <kkp7125@gmail.com>
2025-11-17 21:49:54 +0900

    command group padding

--- src/ui/command.tsx
+++ src/ui/command.tsx
@@ -127,6 +127,7 @@ function CommandGroup({
       data-slot="command-group"
       className={cn(
         'text-foreground [&_[cmdk-group-heading]]:text-muted-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium',
+        'p-2',
         className
       )}
       {...props}
