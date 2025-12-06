9446b73fe140860a24d387c9369bdfeafdba5fdb
hhhjin <hey@hyeongjin.com>
2025-12-06 17:30:47 +0900

    feat: hide editor header on scroll (#247)
    * feat: hide editor header on scroll
    
    - Removed unnecessary imports and components from the Editor, consolidating the header functionality into a new Header component.
    - Added a new editor store to manage scrolling state and handle scroll events.
    - Updated the ToggleButton to reflect scrolling state changes.
    - Adjusted styles for better layout and responsiveness in the EditorContent.
    
    * fix

---
+++ src/store/editor-store.tsx
@@ -0,0 +1,27 @@
+import { create } from 'zustand'
+
+type EditorStore = {
+  isScrolling: boolean
+  scrollTimeoutId: NodeJS.Timeout | null
+  handleScroll: () => void
+}
+
+export const useEditorStore = create<EditorStore>((set, get) => ({
+  isScrolling: false,
+  scrollTimeoutId: null,
+  handleScroll: () => {
+    const { scrollTimeoutId } = get()
+
+    if (scrollTimeoutId) {
+      // Clear existing timeout if any
+      clearTimeout(scrollTimeoutId)
+    }
+
+    // Set new timeout
+    const timeoutId = setTimeout(() => {
+      set({ isScrolling: false, scrollTimeoutId: null })
+    }, 300)
+
+    set({ isScrolling: true, scrollTimeoutId: timeoutId })
+  },
+}))
