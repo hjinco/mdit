773cbb8931495e0cc53db96419f5460f8e0df7b4
hhhjin <kkp7125@gmail.com>
2025-12-12 21:31:39 +0900

    fix

--- src/components/system-tray/system-tray.tsx
+++ src/components/system-tray/system-tray.tsx
@@ -69,6 +69,7 @@ export function SystemTray() {
     const closeListener = appWindow.listen('tauri://close-requested', () => {
       trayPromise.then((tray) => {
         tray.close()
+        trayInstance = null
       })
     })
 
