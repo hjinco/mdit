268953450c52e929524a9f66a2bc944867cf100d
hhhjin <kkp7125@gmail.com>
2026-02-02 10:21:10 +0900

    feat: remove TrailingBlockPlugin

--- src/components/editor/plugins/utils-kit.ts
+++ src/components/editor/plugins/utils-kit.ts
@@ -1,16 +1,10 @@
-import { ExitBreakPlugin, TrailingBlockPlugin } from 'platejs'
+import { ExitBreakPlugin } from 'platejs'
 
 export const UtilsKit = [
   ExitBreakPlugin.configure({
     shortcuts: {
       insert: { keys: 'mod+enter' },
       insertBefore: { keys: 'mod+shift+enter' },
-    },
-  }),
-  TrailingBlockPlugin.configure({
-    options: {
-      type: 'p',
-      exclude: ['blockquote'],
     },
   }),
 ]
