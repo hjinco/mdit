2d71adcaf61711d4b34cc9dba5a24ed4322d65ed
hhhjin <kkp7125@gmail.com>
2026-02-26 12:47:02 +0900

    perf: lazily render tree node children

--- apps/desktop/src/components/file-explorer/tree/tree-node.tsx
+++ apps/desktop/src/components/file-explorer/tree/tree-node.tsx
@@ -8,27 +8,30 @@ export function TreeNode(props: TreeNodeProps) {
 	const { entry } = props
 
 	if (entry.isDirectory) {
-		const childrenTree = entry.children?.map((child) => (
-			<TreeNode
-				key={child.path}
-				entry={child}
-				activeTabPath={props.activeTabPath}
-				depth={props.depth + 1}
-				expandedDirectories={props.expandedDirectories}
-				onDirectoryClick={props.onDirectoryClick}
-				onEntryPrimaryAction={props.onEntryPrimaryAction}
-				onEntryContextMenu={props.onEntryContextMenu}
-				selectedEntryPaths={props.selectedEntryPaths}
-				renamingEntryPath={props.renamingEntryPath}
-				aiRenamingEntryPaths={props.aiRenamingEntryPaths}
-				onRenameSubmit={props.onRenameSubmit}
-				onRenameCancel={props.onRenameCancel}
-				pendingNewFolderPath={props.pendingNewFolderPath}
-				onNewFolderSubmit={props.onNewFolderSubmit}
-				onNewFolderCancel={props.onNewFolderCancel}
-				onCollectionViewOpen={props.onCollectionViewOpen}
-			/>
-		))
+		const isExpanded = props.expandedDirectories.includes(entry.path)
+		const childrenTree = isExpanded
+			? entry.children?.map((child) => (
+					<TreeNode
+						key={child.path}
+						entry={child}
+						activeTabPath={props.activeTabPath}
+						depth={props.depth + 1}
+						expandedDirectories={props.expandedDirectories}
+						onDirectoryClick={props.onDirectoryClick}
+						onEntryPrimaryAction={props.onEntryPrimaryAction}
+						onEntryContextMenu={props.onEntryContextMenu}
+						selectedEntryPaths={props.selectedEntryPaths}
+						renamingEntryPath={props.renamingEntryPath}
+						aiRenamingEntryPaths={props.aiRenamingEntryPaths}
+						onRenameSubmit={props.onRenameSubmit}
+						onRenameCancel={props.onRenameCancel}
+						pendingNewFolderPath={props.pendingNewFolderPath}
+						onNewFolderSubmit={props.onNewFolderSubmit}
+						onNewFolderCancel={props.onNewFolderCancel}
+						onCollectionViewOpen={props.onCollectionViewOpen}
+					/>
+				))
+			: undefined
 
 		return <DirectoryTreeNode {...props} childrenTree={childrenTree} />
 	}
