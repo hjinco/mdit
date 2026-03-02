a0a5ece3254e865a873d639a8728120566c22b0e
hhhjin <kkp7125@gmail.com>
2026-03-02 16:18:38 +0900

    refactor(image): format

--- apps/desktop/src-tauri/src/commands/image.rs
+++ apps/desktop/src-tauri/src/commands/image.rs
@@ -1,5 +1,7 @@
 #[tauri::command]
-pub fn get_image_properties(path: String) -> Result<mdit_image_processing::ImageProperties, String> {
+pub fn get_image_properties(
+    path: String,
+) -> Result<mdit_image_processing::ImageProperties, String> {
     mdit_image_processing::get_image_properties(&path)
 }
 
@@ -8,7 +10,9 @@ pub async fn edit_image(
     input_path: String,
     options: mdit_image_processing::ImageEditOptions,
 ) -> Result<String, String> {
-    tauri::async_runtime::spawn_blocking(move || mdit_image_processing::edit_image(&input_path, options))
-        .await
-        .map_err(|error| error.to_string())?
+    tauri::async_runtime::spawn_blocking(move || {
+        mdit_image_processing::edit_image(&input_path, options)
+    })
+    .await
+    .map_err(|error| error.to_string())?
 }
